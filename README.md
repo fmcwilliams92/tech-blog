f60459c1177b62e5d83ee8044220ef51fe3f90c4
Fredric McWilliams <fredmcwilliams92@gmail.com>
2021-06-24 21:23:32 -0500

    setup the server file and working on the handlebars

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # tech-blog
+
+## Description
+This is a tech based blog site that allows users to create accounts, login, post, comment and delete said posts/comments at their liezure.
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connections');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening on port 3001"));
+});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
